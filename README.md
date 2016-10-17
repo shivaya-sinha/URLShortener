e0cbabad427beebd81d7ac522b52db30c60cb90e
shivaya-sinha <shivaya-sinha11@gmail.com>
2016-10-17 19:12:55 +0530

    Created this script

---
+++ README.md
@@ -0,0 +1,12 @@
+#URL Shortener Python Script
+
+This is a simple python script to shotren long urls easily. 
+The long URLs can be entred as the command line args and the shortened url is given as an output.
+
+##Usage 
+`pyhton shurl.py http://github.com/shivaya-sinha/xyzRepository`
+
+Output: `http://goo.gl/xxxxx`
+
+##Contributing 
+Pull requests are welcomed. Please give proper names to variables.
---
+++ shurl.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+ 
+from sys import argv
+import httplib2
+import simplejson as json
+ 
+API_KEY = ENTER_YOUR_API_KEY_HERE           
+ 
+def shurl(longUrl):
+    
+    try: API_KEY
+    except NameError:
+ 	apiUrl = 'https://www.googleapis.com/urlshortener/v1/url'    
+    else:
+        apiUrl = 'https://www.googleapis.com/urlshortener/v1/url?key=%s' % API_KEY
+    
+    headers = {"Content-type": "application/json"}
+    data = {"longUrl": longUrl}
+    h = httplib2.Http('.cache')
+    try:
+        headers, response = h.request(apiUrl, "POST", json.dumps(data), headers)
+	print response 
+        short_url = json.loads(response)['id']
+ 
+    except Exception, e:
+        print "unexpected error %s" % e
+    return short_url
+		 
+if __name__ == '__main__':
+    short_url = shurl(argv[1])
+    print "**************************"
+    print short_url
+    print "**************************\n"
+
